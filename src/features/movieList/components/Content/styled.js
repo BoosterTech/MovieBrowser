167963bb62d6f8497fba09c040285aca181290ff
Dariusz Podczasik <boostertech@mail.com>
2024-03-25 21:41:10 +0000

    Content container for movies tiles updated

--- src/features/movieList/components/Content/styled.js
+++ src/features/movieList/components/Content/styled.js
@@ -2,23 +2,17 @@ import styled from "styled-components";
 
 export const ContentWrapper = styled.section`
   max-width: 1368px;
-  max-height: 1391px;
+  height: auto;
   top: 150px;
   left: 276px;
-  gap: 0px;
-  opacity: 0px;
   color: ${({ theme }) => theme.color.Black};
   padding: 0;
-  margin: 56px auto 0 auto;
+  margin: 56px auto 40px auto;
 `;
 
 export const ContentHeader = styled.h1`
   max-width: 285px;
   max-height: 43px;
-  top: 150px;
-  left: 276px;
-  gap: 0px;
-  opacity: 0px;
   font-family: Poppins;
   font-size: 36px;
   font-weight: 600;
@@ -30,10 +24,10 @@ export const ContentHeader = styled.h1`
 `;
 
 export const TilesContainer = styled.div`
-  width: Hug (1, 368px) px;
-  height: Hug (650px) px;
-  top: 217px;
-  left: 276px; 
-  display: flex;
+  display: grid;
+  grid-template-columns: repeat(4, 1fr);
+  gap: 24px;
+  height: auto;
+  max-width: 368px;
   margin: 24px 0;
 `;
