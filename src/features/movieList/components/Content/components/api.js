f9f7209dfcc0682e2d8b3c2b16fe7ba30ba44444
Dariusz Podczasik <boostertech@mail.com>
2024-03-25 20:59:51 +0000

    api.js created, fetch+await+async used

--- src/features/movieList/components/Content/components/api.js
+++ src/features/movieList/components/Content/components/api.js
@@ -1,19 +1,26 @@
 export const fetchMovies = async () => {
   try {
     const response = await fetch(
-      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1?api_key=3b260196bc9bbb0df7bd747cf01357c0"
+      "https://api.themoviedb.org/3/movie/popular?language=en-US&page=1",
+      {
+        headers: {
+          Authorization:
+            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjI2MDE5NmJjOWJiYjBkZjdiZDc0N2NmMDEzNTdjMCIsInN1YiI6IjY2MDAwZmZjNjJmMzM1MDE2NDUxNWJhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j5BdMVijb7g_8sAaxu9WLzzFLGen6qB1lNYLha-96Tw",
+          Accept: "application/json",
+        },
+      }
     );
 
     if (!response.ok) {
       throw new Error(response.statusText);
+      console.log("response error");
     }
 
-    const { meta, data } = await response.json();
+    const { meta, results } = await response.json();
 
     return {
       state: "success",
-      meta,
-      data,
+      data: results,
     };
   } catch {
     return {
