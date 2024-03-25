565c0102ea8512cc88f0c42b8f114f5119c689db
Dariusz Podczasik <boostertech@mail.com>
2024-03-25 21:53:43 +0000

    MovieTile props from api

--- src/features/movieList/components/Content/index.js
+++ src/features/movieList/components/Content/index.js
@@ -1,12 +1,27 @@
 import MovieTile from "./components/MovieTile";
 import { ContentHeader, ContentWrapper, TilesContainer } from "./styled";
+import { useMoviesData } from "./components/useMoviesData";
 
 export const Content = () => {
+  const moviesData = useMoviesData();
+
   return (
     <ContentWrapper>
       <ContentHeader>Popular Movies</ContentHeader>
       <TilesContainer>
-        <MovieTile />
+        {moviesData.state === "success" &&
+          moviesData.data.map((movie) => {
+            return (
+              <MovieTile
+                key={movie.id}
+                title={movie.title}
+                // image={movie.poster_path}
+                year={movie.release_date.substring(0,4)}
+                rate={movie.vote_average}
+                vote={movie.vote_count}
+              />
+            );
+          })}
       </TilesContainer>
     </ContentWrapper>
   );
