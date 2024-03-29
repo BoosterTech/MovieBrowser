5b872f4c2f9a08e47a2a20587fac53bb40bf910d
Dariusz Podczasik <boostertech@mail.com>
2024-03-29 15:34:27 +0000

    file removed

--- src/common/API_URL.js
+++
@@ -1,5 +0,0 @@
-export const apiMoviePopular =
-  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=";
-
-export const apiMovieImage =
-  "https://api.themoviedb.org/3/movie/movie_id/images";
