5ec977e2561c86cc3461ab72ef8a55562e06fc85
Dariusz Podczasik <boostertech@mail.com>
2024-03-29 20:33:26 +0000

    handlers for page nr changing created in pagination

--- src/features/movieList/components/Footer/index.js
+++ src/features/movieList/components/Footer/index.js
@@ -1,4 +1,4 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import {
   Wrapper,
   Container,
@@ -10,37 +10,74 @@ import {
   Section,
   DisabledText,
 } from "./styled";
-import { selectMovieStateValue } from "../../../../Redux_store/moviesSlice";
+import {
+  nextPage,
+  previousPage,
+  selectMovieStateValue,
+  selectMoviesPageNr,
+  setFirstPage,
+  setLastPage,
+  updateState,
+} from "../../../../Redux_store/moviesSlice";
 
 export const Footer = () => {
   const state = useSelector(selectMovieStateValue);
+  const page = useSelector(selectMoviesPageNr);
+  const dispatch = useDispatch();
+
+  const handleNextPage = () => {
+    if (page === 500) {
+      return;
+    } else {
+      dispatch(nextPage());
+      dispatch(updateState("loading"));
+    }
+  };
+  const handlePreviousPage = () => {
+    if (page == 1) {
+      return;
+    } else {
+      dispatch(previousPage());
+      dispatch(updateState("loading"));
+    }
+  };
+
+  const handleLastPage = () => {
+    dispatch(setLastPage());
+    dispatch(updateState("loading"));
+  };
+
+  const handleFirstPage = () => {
+    dispatch(setFirstPage());
+    dispatch(updateState("loading"));
+  };
 
   return (
     state !== "loading" && (
       <Wrapper>
         <Container>
           <Section>
-            <Button disabled>
+            <Button disabled={page === 1} onClick={handleFirstPage}>
               <VectorLeft />
               First
             </Button>
-            <Button disabled>
+            <Button disabled={page === 1} onClick={handlePreviousPage}>
               <VectorLeft />
               Previous
             </Button>
           </Section>
           <PageChange>
             <DisabledText>Page</DisabledText>
-            <Number>1</Number>
+            <Number>{page}</Number>
             <DisabledText>of</DisabledText>
             <Number>500</Number>
           </PageChange>
           <Section>
-            <Button>
+            <Button disabled={page === 500} onClick={handleNextPage}>
               Next
               <VectorRight />
             </Button>
-            <Button>
+            <Button disabled={page === 500} onClick={handleLastPage}>
               Last
               <VectorRight />
             </Button>
