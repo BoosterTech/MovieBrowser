aa63dcfc64f8dbfee25ac8e53d92fe4cd423a7b1
Dariusz Podczasik <boostertech@mail.com>
2024-03-29 11:03:16 +0000

    default path added to main Route

--- src/App.js
+++ src/App.js
@@ -1,23 +1,22 @@
-import { HashRouter, Switch, NavLink, Route, Redirect } from "react-router-dom";
+import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
 import MovieList from "./core/MovieList";
 import People from "./features/personList";
 import { toMovieListPage, toPeople } from "./routes";
 import { Header } from "./common/Header";
 
 export default () => (
-    <HashRouter>
-        <Header />
-        
-        <Switch>
-            <Route path={toPeople()}>
-                <People />
-            </Route>
-            <Route path={toMovieListPage()}>
-                <MovieList />
-            </Route>
-            <Route >
-                <Redirect to={toMovieListPage()} />
-            </Route>
-        </Switch>
-    </HashRouter>
+  <HashRouter>
+    <Header />
+    <Switch>
+      <Route path={toPeople()}>
+        <People />
+      </Route>
+      <Route path={toMovieListPage()}>
+        <MovieList />
+      </Route>
+      <Route path="/">
+        <Redirect to={toMovieListPage()} />
+      </Route>
+    </Switch>
+  </HashRouter>
 );
