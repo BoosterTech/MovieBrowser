db6b8c2c258f4915098247442095d759ec31fd2d
Dariusz Podczasik <boostertech@mail.com>
2024-03-30 21:24:11 +0000

    PopularMoviesURL added

--- src/assets/API_URL.js
+++ src/assets/API_URL.js
@@ -2,3 +2,5 @@ export const apiMoviePopular =
   "https://api.themoviedb.org/3/movie/popular?language=en-US&page=";
 
 export const apiBaseUrl = "https://api.themoviedb.org/3/";
+
+export const PopularMoviesURL = "${apiMoviePopular}${moviePage}";
