dd8ccab2c261b604fd82d8a905f077191e561693
Dariusz Podczasik <boostertech@mail.com>
2024-03-31 23:13:03 +0100

    <ImageDefault> component created for personList

---
+++ src/features/personList/components/ImageDefault/styled.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const DefaultImageWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: grey;
+  width: 176px;
+  height: 231px;
+  margin: auto;
+  border-radius: 5px;
+`;
