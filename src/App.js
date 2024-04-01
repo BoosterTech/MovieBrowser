070d26e500ed9cab576abbc773684333acbc1680
Darek Podczasik <boostertech@mail.com>
2024-04-02 00:49:34 +0100

    Pagination added to each route seperately

--- src/App.js
+++ src/App.js
@@ -4,37 +4,30 @@ import { Navigation } from "./common/Navigation";
 import { MovieListPage } from "./features/movieList";
 import PersonList from "./features/personList";
 
-import {
-  selectSettingLoadingValue,
-  selectSettingPageStateValue,
-} from "./Redux_store/settingSlice";
-import { useSelector } from "react-redux";
 import Pagination from "./common/Pagination";
 import PersonDetails from "./features/personDetails";
 
 const App = () => {
-  const pageState = useSelector(selectSettingPageStateValue);
-  const loadingState = useSelector(selectSettingLoadingValue);
-
   return (
     <HashRouter exact basename="/MovieBrowser">
       <Navigation />
       <Switch>
         <Route exact path={toMovieListPage()}>
           <MovieListPage />
+          <Pagination />
+        </Route>
+        <Route exact path={toPeople()}>
+          <PersonList />
+          <Pagination />
         </Route>
         <Route exact path={toProfile()}>
           <PersonDetails />
         </Route>
-        <Route exact path={toPeople()}>
-          <PersonList />
-        </Route>
         <Route exact path="/">
           <Redirect to={toMovieListPage()} />
+          <Pagination />
         </Route>
       </Switch>
-      {(pageState === "movies" || pageState === "people") &&
-        loadingState === "success" && <Pagination />}
     </HashRouter>
   );
   p;
