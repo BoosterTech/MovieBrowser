80f4df130f7abd0d64a63f006933f673f361ab7e
Darek Podczasik <boostertech@mail.com>
2024-04-02 15:03:59 +0100

    Reapply "NavLink & route simplyfied  only for purpose of testing Profile template"
    This reverts commit 279377733d04fe4bc75402327e8c21b31c02a39c.

--- src/routes.js
+++ src/routes.js
@@ -1,4 +1,5 @@
 export const toMovieListPage = () => "/movieListPage";
 export const toPeople = () => "/people";
-export const toProfile = ({ id } = { id: ":id" }) =>
-  `profile/${id}`;
+// export const toProfile = ({ id } = { id: ":id" }) =>
+//   `profile/${id}`;
+export const toProfile = () => "/profile";
