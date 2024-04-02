d82501b21b6dc3d818276806bc3b95fcc6e68197
Arkadiusz0809 <arek.torun12@gmail.com>
2024-04-02 13:38:41 +0200

    changed sizes value

--- src/styles/Theme.js
+++ src/styles/Theme.js
@@ -1,7 +1,7 @@
 export const theme = {
   color: {
     Light_grey: "#F5F5FA", // body background
-    Black: "#18181B", // MovieList content's text
+    Woodsmoke: "#18181B", // MovieList content's text
     Mystic: "#E4E6F0",
     Waterloo: "#7E839A",
     PattensBlue: "#D6E4FF",
@@ -9,17 +9,15 @@ export const theme = {
     black: "#000000",
     darkergray: "rgba(126, 131, 154, 1)",
   },
-  breakpoint: {
-    mobile: "320px",
-    tablet: "1024px",
-    phone: "767px",
+  breakpoints: {
+    mobileMin: 390,
+    mobileMax: 767,
+    tabletVerticalMax: 991,
+    tabletHorizontalMax: 1199,
   },
   size: {
     maxwidth: "1200px",
   },
-  media: {
-    phone: "767px",
-    tablet: "1024px",
-},
+
 
 };
