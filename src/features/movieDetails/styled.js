6faebc29bcb7d6885d09bb79dcecf4e741ac1e8e
Darek Podczasik <boostertech@mail.com>
2024-04-04 17:25:51 +0100

    <TopTileContainer/> & <MovieDetailsWrapper/> styles added

---
+++ src/features/movieDetails/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const TopTileContainer = styled.div`
+  max-width: 100vw;
+  height: 700px;
+  color: white;
+  border: 1px solid grey;
+  margin: 0;
+`;
+
+export const MovieDetailsWrapper = styled.div`
+  width: 1288px;
+  height: 544px;
+  padding: 40px;
+  border: 1px solid black;
+  margin: 64px auto;
+`;
