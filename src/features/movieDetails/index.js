c39eaeeeb353fb9c5621f7b56d714d28212ff928
Darek Podczasik <boostertech@mail.com>
2024-04-04 17:31:39 +0100

    <MovieDetails/> page component created. <TopTileContainer/>, <MovieDetailsWrapper/>, sections Cast & Crew with default tiles added (first picture with <Navlink/> property to ProfileDetails page

--- src/features/movieDetails/index.js
+++ src/features/movieDetails/index.js
@@ -1,6 +1,12 @@
 import { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { setLoadingState } from "../../Redux_store/settingSlice";
+import { CastCrewWrapper, Header } from "../personDetails/styled";
+import { TilesContainer } from "../movieList/styled";
+import { MovieDetailsWrapper, TopTileContainer } from "./styled";
+import { PersonTile } from "../personList/components/PersonTile";
+import { NavLink } from "react-router-dom";
+import { toProfile } from "../../routes";
 
 const MovieDetails = () => {
   const dispatch = useDispatch();
@@ -9,7 +15,43 @@ const MovieDetails = () => {
     dispatch(setLoadingState("loading"));
   }, []);
 
-  return <p>MovieDetails</p>;
+  return (
+    <>
+      <TopTileContainer />
+      <MovieDetailsWrapper />
+
+      <CastCrewWrapper>
+        <Header>Cast</Header>
+        <TilesContainer personTile>
+          <NavLink to={toProfile()} style={{ textDecoration: "none" }}>
+            <PersonTile />
+          </NavLink>
+          <PersonTile />
+          <PersonTile />
+          <PersonTile />
+          <PersonTile />
+          <PersonTile />
+          <PersonTile />
+          <PersonTile />
+          <PersonTile />
+          <PersonTile />
+          <PersonTile />
+        </TilesContainer>
+      </CastCrewWrapper>
+      <CastCrewWrapper second={`${true}`}>
+        <Header>Crew</Header>
+        <TilesContainer personTile>
+          <NavLink to={toProfile()} style={{ textDecoration: "none" }}>
+            <PersonTile />
+          </NavLink>
+          <PersonTile />
+          <PersonTile />
+          <PersonTile />
+          <PersonTile />
+        </TilesContainer>
+      </CastCrewWrapper>
+    </>
+  );
 };
 
 export default MovieDetails;
