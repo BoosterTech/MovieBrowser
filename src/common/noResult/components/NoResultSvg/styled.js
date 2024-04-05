550076e509967da9fb04081163035a0a0c12a9a1
Darek Podczasik <boostertech@mail.com>
2024-04-05 14:14:33 +0100

    svg image centralised on the screen, styles adjusted

--- src/common/noResult/components/NoResultSvg/styled.js
+++ src/common/noResult/components/NoResultSvg/styled.js
@@ -1,6 +1,8 @@
 import { styled } from "styled-components";
 
 export const StyledSvg = styled.svg`
-  width: 668px;
-  height: 509px;
+  display: block;
+  max-width: 668px;
+  max-height: 509px;
+  margin: auto;
 `;
