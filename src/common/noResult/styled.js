cbb29e004bd1e5bd209752aceb9e3603e2095330
Darek Podczasik <boostertech@mail.com>
2024-04-05 14:13:40 +0100

    <MessagePargraph/> styled adjusted to figma values

--- src/common/noResult/styled.js
+++ src/common/noResult/styled.js
@@ -4,8 +4,8 @@ export const MessageParagraph = styled.h1`
   width: auto;
   height: 43px;
   font-weight: 600;
-  font-size: 36px;
+  font-size:36px;
   line-height: 43.2px;
   color: black;
-  margin: 150px auto auto 276px;
+  margin: 56px auto auto 276px;
 `;
