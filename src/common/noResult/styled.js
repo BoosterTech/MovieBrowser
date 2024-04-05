da50d42984aabbcf01c3db37e3c66fc7473f9ab9
Darek Podczasik <boostertech@mail.com>
2024-04-05 14:02:27 +0100

    noResult svg added to images, <NoResultSvg/> component created add initially styled

---
+++ src/common/noResult/styled.js
@@ -0,0 +1,11 @@
+import { styled } from "styled-components";
+
+export const MessageParagraph = styled.h1`
+  width: auto;
+  height: 43px;
+  font-weight: 600;
+  font-size: 36px;
+  line-height: 43.2px;
+  color: black;
+  margin: 150px auto auto 276px;
+`;
