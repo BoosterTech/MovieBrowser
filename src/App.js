4d36179e7e1612e2aef7c10ea336efcbb783e1d1
Darek Podczasik <boostertech@mail.com>
2024-04-05 14:01:12 +0100

    routes added to App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
-import { toMovieDetails, toMovieListPage, toPeople, toProfile } from "./routes";
+import {
+  toError,
+  toMovieDetails,
+  toMovieListPage,
+  toNoResult,
+  toPeople,
+  toProfile,
+} from "./routes";
 import { Navigation } from "./common/Navigation";
 import { MovieListPage } from "./features/movieList";
 import PersonList from "./features/personList";
 import Pagination from "./common/Pagination";
 import ProfileDetails from "./features/personDetails";
 import MovieDetails from "./features/movieDetails";
+import NoResultPage from "./common/noResult";
 
 const App = () => {
   return (
@@ -23,12 +31,16 @@ const App = () => {
         <Route exact path={toProfile()}>
           <ProfileDetails />
         </Route>
-        <Route exact path="/">
-          <Redirect to={toMovieListPage()} />
-          <Pagination />
-        </Route>
         <Route exact path={toMovieDetails()}>
           <MovieDetails />
+        </Route>
+        <Route exact path={toNoResult()}>
+          <NoResultPage />
+        </Route>
+        <Route exact path={toError()}></Route>
+        <Route path="/">
+          <Redirect to={toMovieListPage()} />
+          <Pagination />
         </Route>
       </Switch>
     </HashRouter>
