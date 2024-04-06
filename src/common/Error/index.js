a61a75c5fcbb388702022db9d709d79e0afa6198
Darek Podczasik <boostertech@mail.com>
2024-04-06 13:04:15 +0100

    <DangerIcon/> & all other styled components added to <ErrorPage/>

--- src/common/Error/index.js
+++ src/common/Error/index.js
@@ -1,5 +1,27 @@
+import DangerIcon from "./components";
+import {
+  ErrorButton,
+  ErrorButtonText,
+  ErrorMessageWrapper,
+  MessageHeader,
+  MessageInfo,
+} from "./styled";
+
 const ErrorPage = () => {
-  return <></>;
+  return (
+    <>
+      <DangerIcon />
+      <ErrorMessageWrapper>
+        <MessageHeader>Ooops! Something went wrong...</MessageHeader>
+        <MessageInfo>
+          Please check your network connection and try again
+        </MessageInfo>
+        <ErrorButton>
+          <ErrorButtonText>Back to home page</ErrorButtonText>
+        </ErrorButton>
+      </ErrorMessageWrapper>
+    </>
+  );
 };
 
 export default ErrorPage;
