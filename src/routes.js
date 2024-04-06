293bcdec60eb646b75b2e7f938e77f8803d5c1ca
Darek Podczasik <boostertech@mail.com>
2024-04-06 21:14:42 +0100

    route path created in routes.js

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 export const toMovieListPage = () => "/movieListPage";
 export const toPeople = () => "/people";
-// export const toProfile = ({ id } = { id: ":id" }) =>
-//   `profile/${id}`;
-export const toProfile = () => "/profile";
+export const toProfile = ({ id } = { id: ":id" }) => `/people/${id}`;
+// export const toProfile = () => "/profile";
 export const toMovieDetails = () => "/movieDetails";
