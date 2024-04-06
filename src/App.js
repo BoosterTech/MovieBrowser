7275e4392827d59f22f789704959b8f594fef9d8
Darek Podczasik <boostertech@mail.com>
2024-04-06 14:59:34 +0100

    <SearchPage/> added to <Route> in App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import { HashRouter, Route, Redirect, Switch } from "react-router-dom";
-import { toMovieDetails, toMovieListPage, toPeople, toProfile } from "./routes";
+import { toMovieDetails, toMovieListPage, toPeople, toProfile, toSearch } from "./routes";
 import { Navigation } from "./common/Navigation";
 import { MovieListPage } from "./features/movieList";
 import PersonList from "./features/personList";
 import Pagination from "./common/Pagination";
 import ProfileDetails from "./features/personDetails";
 import MovieDetails from "./features/movieDetails";
+import SearchPage from "./common/SearchPage";
 
 const App = () => {
   return (
@@ -23,12 +24,15 @@ const App = () => {
         <Route exact path={toProfile()}>
           <ProfileDetails />
         </Route>
-        <Route exact path="/">
-          <Redirect to={toMovieListPage()} />
-          <Pagination />
-        </Route>
         <Route exact path={toMovieDetails()}>
           <MovieDetails />
+        </Route>
+        <Route exact path={toSearch()}>
+          <SearchPage />
+        </Route>
+        <Route path="/">
+          <Redirect to={toMovieListPage()} />
+          <Pagination />
         </Route>
       </Switch>
     </HashRouter>
