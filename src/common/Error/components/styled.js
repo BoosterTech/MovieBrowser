5ec2cd0a6f99effcda1f780a5d2d04ab566f90b2
Darek Podczasik <boostertech@mail.com>
2024-04-06 13:02:05 +0100

    <DangerIcon/> svg created & styled

---
+++ src/common/Error/components/styled.js
@@ -0,0 +1,8 @@
+import { styled } from "styled-components";
+
+export const StyledDangerIcon = styled.svg`
+  display: block;
+  max-width: 120px;
+  max-height: 120px;
+  margin: 174px auto 0 auto;
+`;
