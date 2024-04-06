46636a3066727164b3635cd1f6ed1cee51cdceaa
Darek Podczasik <boostertech@mail.com>
2024-04-06 14:58:39 +0100

    <SearchPage/> created & styled

---
+++ src/common/SearchPage/styled.js
@@ -0,0 +1,12 @@
+import { styled } from "styled-components";
+
+export const MessageHeader = styled.h1`
+  width: 602px;
+  height: 43px;
+  font-size: 36px;
+  font-weight: 600;
+  line-height: 43.2px;
+  text-align: left;
+  color: black;
+  margin: 80px auto auto 276px;
+`;
