78acfd21e1a46d0916e592fc4abf027696d36d7e
Arkadiusz0809 <arek.torun12@gmail.com>
2024-04-08 13:09:23 +0200

    prepared component

--- src/features/movieDetails/components/TopTileContainer/index.js
+++ src/features/movieDetails/components/TopTileContainer/index.js
@@ -1,7 +1,39 @@
+import { ReactComponent as Star } from "./../../../../assets/images/star.svg";
+import { ReactComponent as Poster } from "./../../../../assets/images/Poster_big.svg";
+import { ReactComponent as Pleksa } from "./../../../../assets/images/Pleksa.svg";
+import {
+    Wrapper,
+    ShadowContainer,
+    IconPoster,
+    Title,
+    RatingWrapper,
+    IconStar,
+    RateContainer,
+    VoteContainer,
+    ScoreLimit,
+} from "./styled";
 
+export const TopTileContainer = ({
 
-export const TopTileContainer = () => {
-    <>
-    </>
+    title = "Mulan long title",
+    rate = 7.8,
+    vote = 3876,
+}) => {
+    return (
+        <Wrapper>
+            <ShadowContainer>
+                <IconPoster as={Poster} />
+                <Title>{title}</Title>
+                <RatingWrapper>
+                    <IconStar as={Star} />
+                    <RateContainer>{rate.toFixed(1)}</RateContainer>
+                    <ScoreLimit>/10</ScoreLimit>
+                </RatingWrapper>
+                <VoteContainer>
+                    {vote} {vote > 1 ? "votes" : "vote"}
+                </VoteContainer>
+            </ShadowContainer>
+        </Wrapper>
+    )
 }
 
