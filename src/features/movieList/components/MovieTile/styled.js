6eae9af278dddee597ee374077226716eed4b51a
Arkadiusz0809 <arek.torun12@gmail.com>
2024-04-08 13:10:04 +0200

    fixed line

--- src/features/movieList/components/MovieTile/styled.js
+++ src/features/movieList/components/MovieTile/styled.js
@@ -19,8 +19,7 @@ export const MovieTileWrapper = styled.div`
     cursor: pointer;
   }
 
-  @media (max-width: ${({ theme }) =>
-      theme.breakpoints.tabletHorizontalMax}px) {
+  @media (max-width: ${({ theme }) =>theme.breakpoints.tabletHorizontalMax}px) {
     margin: 0 auto;
 
     &:hover {
