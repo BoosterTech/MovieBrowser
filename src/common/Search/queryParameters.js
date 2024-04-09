62cbded97f5c2cffa20db2508e8b20bd9011b6fc
Darek Podczasik <boostertech@mail.com>
2024-04-09 21:19:37 +0100

    useQueryParameter & useReplaceQueryParameter hooks created together with queryParameters.js file

---
+++ src/common/Search/queryParameters.js
@@ -0,0 +1,23 @@
+import { useLocation } from "react-router-dom";
+
+export const useQueryParameter = (key) => {
+  const location = useLocation();
+  const searchParams = new URLSearchParams(location.search);
+
+  return searchParams.get(key);
+};
+
+export const useReplaceQueryParameter = () => {
+  const location = useLocation();
+  const history = useHistory();
+
+  return ({ key, value }) => {
+    const searchParams = new URLSearchParams(location.search);
+
+    value === undefined
+      ? searchParams.delete(key)
+      : searchParams.set(key, value);
+
+    history.push(`${location.pathname}?${searchParams.toString()}`);
+  };
+};
