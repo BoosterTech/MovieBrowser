4c6231fc78ea26c9324381bab7a6253905d9d4dc
Darek Podczasik <boostertech@mail.com>
2024-04-10 19:52:56 +0100

    styles for Populat People Page fixes (Font size, margins)

--- src/features/personList/components/PersonTile/styled.js
+++ src/features/personList/components/PersonTile/styled.js
@@ -5,26 +5,27 @@ export const TileWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  justify-content: center;
-  height: 339x;
+  justify-content: flex-start;
+  height: 339px;
   width: 208px;
-  margin: 0 0 24px 0;
+  margin: 24px 0 24px 0;
   border-radius: 5px;
   color: black;
   box-shadow: 0px 4px 12px 0px #bac7d580;
   animation: ${showin} 1s ease;
   transition: transform 0.5s ease;
+  padding: 0;
 
   &:hover {
     transform: scale(1.05);
     cursor: pointer;
   }
-  
-  @media(max-width: ${({theme}) => theme.breakpoints.desktopMini}px){
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.desktopMini}px) {
     margin: auto;
   }
 
-  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px){
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     width: 136px;
     height: 245px;
   }
@@ -33,26 +34,28 @@ export const TileWrapper = styled.div`
 export const TileImage = styled.img`
   width: 176px;
   height: 231px;
-  margin: auto;
   margin-top: 16px;
+  margin-bottom: 0;
   border-radius: 5px;
-  
-  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px){
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     width: 120px;
     height: 178px;
     margin: 8px;
+    margin-bottom: 12px;
   }
 `;
 
 export const PersonName = styled.div`
   width: 176px;
-  height: 74px;
-  margin:  12px auto;
-  padding-top: 8px;
-  text-align: center;
+  min-height: 29px;
+  font-size: 18px;
   font-weight: 500;
+  line-height: 28.6px;
+  text-align: center;
+  margin: 12px auto 22px auto;
 
-  @media(max-width: ${({theme}) => theme.breakpoints.mobileMax}px){
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
     font-size: 14px;
     line-height: 18.2px;
     max-width: 120px;
