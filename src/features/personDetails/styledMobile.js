4adb25a3c21dce244edc6e9567056213a51a8b6c
Kacper Lewko <k.lewkosga@gmail.com>
2024-04-10 11:27:48 +0200

    added mediaquery to Profile Up Section

--- src/features/personDetails/styledMobile.js
+++ src/features/personDetails/styledMobile.js
@@ -1,7 +1,7 @@
 import { styled } from "styled-components";
 
 export const ForMobile = styled.div`
-  @media (min-width: ${({ theme }) => theme.breakpoints.tabletHorizontalMax}px) {
+  @media (min-width: ${({ theme }) =>theme.breakpoints.tabletHorizontalMax}px) {
     display: none;
   }
 `;
@@ -29,9 +29,7 @@ export const PersonImageDataContainer = styled.div`
   }
 `;
 
-export const DataContainer = styled.div`
-  // border: solid;
-`;
+export const DataContainer = styled.div``;
 
 export const BirthInfoMobile = styled.div`
   display: flex;
@@ -45,22 +43,23 @@ export const BirthInfoMobile = styled.div`
 `;
 
 export const HeaderMobile = styled.h1`
-  height: 43px;
+  height: 18px;
   font-weight: 600;
   font-size: 26px;
   line-height: 43.2px;
-  margin: 0 0 24px 0;
+  margin: 0 0 30px 0;
 
-  @media(max-width: ${({theme}) => theme.breakpoints.mobilePeopleMedium}px){
-  font-size: 14px;
-  font-weight: 500;
-  margin: 4px 6px 16px 0px;
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobilePeopleMedium}px) {
+    font-size: 14px;
+    font-weight: 500;
+    margin: 0 0 16px 0;
+    line-height: 18.2px;
+    
   }
 `;
 
 export const ParagraphMobile = styled.p`
   margin: 0 0 8px 0;
-  font-size: 12px;
   line-height: 14.4px;
 
   ${(props) => props.grey && `color: grey;`}
@@ -71,7 +70,9 @@ export const ParagraphMobile = styled.p`
       font-size:20px;
   `}
 
-@media(max-width: ${({ theme }) => theme.breakpoints.mobilrPeopleMedium}px) {
+@media(max-width: ${({ theme }) => theme.breakpoints.mobilePeopleMedium}px) {
+    margin: 0 0 8px 0;
+    font-size: 12px;
   }
 `;
 
