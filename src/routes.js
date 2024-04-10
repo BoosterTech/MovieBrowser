0b5a92395c786611670d8d10f6df733de674e72c
Darek Podczasik <boostertech@mail.com>
2024-04-10 22:31:50 +0100

    rote changed in toMovieListPage

--- src/routes.js
+++ src/routes.js
@@ -1,8 +1,7 @@
-export const toMovieListPage = () => "/movieListPage";
+export const toMovieListPage = () => "/movies";
 export const toPeople = () => "/people";
 export const toProfile = ({ id } = { id: ":id" }) => `/people/${id}`;
 export const toMovieDetails = () => "/movieDetails";
 export const toSearch = () => "/searching";
 export const toNoResult = () => "/noResult";
 export const toError = () => "/error";
-
