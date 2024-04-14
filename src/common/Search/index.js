23a732c021b15a037acdf63ac760ed747b41fe70
Darek Podczasik <boostertech@mail.com>
2024-04-14 15:55:53 +0100

    updatePlaceholder() , onBlur(),

--- src/common/Search/index.js
+++ src/common/Search/index.js
@@ -1,32 +1,43 @@
-import { selectSettingPageStateValue } from "../../Redux_store/settingSlice";
+import { useState } from "react";
+import {
+  selectSettingPageStateValue,
+  setSearchState,
+} from "../../Redux_store/settingSlice";
 import { Input, SearchWrapper } from "../Navigation/styled";
 import { useQueryParameter, useReplaceQueryParameter } from "./queryParameters";
 import searchQueryParamName from "./searchQueryParamName";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 
 const Search = () => {
   const query = useQueryParameter(searchQueryParamName);
   const replaceQueryParameter = useReplaceQueryParameter();
   const pageState = useSelector(selectSettingPageStateValue);
+  const [placeholder, setPlaceholder] = useState(
+    pageState === "movies" ? "Search for movies..." : "Search for people..."
+  );
+  const dispatch = useDispatch();
 
   const onInputChange = ({ target }) => {
+    dispatch(setSearchState(true));
     replaceQueryParameter({
       key: searchQueryParamName,
       value: target.value.trim() !== "" ? target.value : undefined,
     });
   };
 
+  const updatePlaceholder = () => {
+    dispatch(setSearchState(false));
+    setPlaceholder(placeholder);
+  };
+
   return (
     <SearchWrapper>
       <Input
         type="text"
-        placeholder={
-          pageState === "movies"
-            ? "Search for movies..."
-            : "Search for people..."
-        }
-        value={query || ""}
+        placeholder={placeholder}
         onChange={onInputChange}
+        onBlur={updatePlaceholder}
+        value={query || ""}
       />
     </SearchWrapper>
   );
