9e0514b23d9ff22ec56c8041ee6402451bda19af
Darek Podczasik <boostertech@mail.com>
2024-04-17 21:04:21 +0100

    SearchPage() & NoResultpage added

--- src/features/movieList/index.js
+++ src/features/movieList/index.js
@@ -17,6 +17,8 @@ import {
 import { toMovieDetails } from "../../routes";
 import { NavLink, useHistory, useLocation } from "react-router-dom";
 import searchQueryParamName from "../../common/Search/searchQueryParamName";
+import SearchPage from "../../common/SearchPage";
+import NoResultPage from "../../common/noResult";
 
 export const MovieListPage = () => {
   const [moviesData, setMoviesData] = useState(null);
@@ -37,7 +39,7 @@ export const MovieListPage = () => {
     const path = location.pathname;
 
     dispatch(setLoadingState("loading"));
-    dispatch(setPageState("movies"));
+    if (!searchState) dispatch(setPageState("movies"));
 
     if (params && path.includes("/movies")) dispatch(setPageNr(Number(params)));
 
@@ -70,52 +72,66 @@ export const MovieListPage = () => {
         if (!responseMovies.ok) {
           throw new Error(responseMovies.statusText());
         }
+
         const { results } = await responseMovies.json();
+
         setMoviesData(results);
         dispatch(setLoadingState("success"));
       } catch (error) {
         dispatch(setLoadingState("error"));
         console.error("Error fetching movies:", error);
+        <errorPage />;
       }
     };
     fetchMovies();
   }, [pageNr, dispatch, myQuery]);
 
   return loadingState === "loading" ? (
-    <LoadingSpinner />
-  ) : (
-    pageState === "movies" && (
-      <ContentWrapper>
-        <ContentHeader>Popular Movies</ContentHeader>
-        <TilesContainer>
-          {moviesData &&
-            moviesData.map((movie) => {
-              const movieGenres = movie.genre_ids.map(
-                (id) => moviesGenres_ids[id]
-              );
-              return (
-                <NavLink
-                  to={toMovieDetails({ id: movie.id })} // Assuming toMovieDetails expects an ID parameter
-                  key={`movie-${movie.id}`}
-                  style={{ textDecoration: "none" }}
-                >
-                  <MovieTile
-                    title={movie.title}
-                    imageSrc={
-                      movie.poster_path
-                        ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
-                        : null
-                    }
-                    category={movieGenres}
-                    year={movie.release_date.substring(0, 4)}
-                    rate={movie.vote_average}
-                    vote={movie.vote_count}
-                  />
-                </NavLink>
-              );
-            })}
-        </TilesContainer>
-      </ContentWrapper>
+    searchState ? (
+      SearchPage(myQuery)
+    ) : (
+      <LoadingSpinner />
     )
+  ) : (
+    (pageState === "movies" || searchState === true) &&
+      (searchState === true && (!moviesData || moviesData.length === 0) ? (
+        NoResultPage(myQuery)
+      ) : (
+        <ContentWrapper>
+          <ContentHeader>
+            {!searchState || myQuery === null
+              ? "Popular Movies"
+              : `Search Results for "${myQuery}"`}
+          </ContentHeader>
+          <TilesContainer>
+            {moviesData &&
+              moviesData.map((movie) => {
+                const movieGenres = movie.genre_ids.map(
+                  (id) => moviesGenres_ids[id]
+                );
+                return (
+                  <NavLink
+                    to={toMovieDetails({ id: movie.id })}
+                    key={`movie-${movie.id}`}
+                    style={{ textDecoration: "none" }}
+                  >
+                    <MovieTile
+                      title={movie.title}
+                      imageSrc={
+                        movie.poster_path
+                          ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+                          : null
+                      }
+                      category={movieGenres}
+                      year={movie.release_date.substring(0, 4)}
+                      rate={movie.vote_average}
+                      vote={movie.vote_count}
+                    />
+                  </NavLink>
+                );
+              })}
+          </TilesContainer>
+        </ContentWrapper>
+      ))
   );
 };
