aa33c3aaae313eefc0f9ba3925d09b91520d3800
Darek Podczasik <boostertech@mail.com>
2024-04-17 20:57:39 +0100

    "searchState" added to Navigation

--- src/common/Navigation/index.js
+++ src/common/Navigation/index.js
@@ -14,7 +14,11 @@ import {
 import { toMovieListPage, toPeople } from "../../routes";
 import { ReactComponent as SearchIcon } from "../../assets/images/SearchIcon.svg";
 import { ReactComponent as VideoIcon } from "../../assets/images/NavVideoIcon.svg";
-import { setBothPages } from "../../Redux_store/settingSlice";
+import {
+  selectSettingSearchValue,
+  setBothPages,
+  setSearchState,
+} from "../../Redux_store/settingSlice";
 import Search from "../Search";
 import { useDispatch, useSelector } from "react-redux";
 import { useEffect, useState } from "react";
@@ -24,15 +28,18 @@ export const Navigation = () => {
   const [activeButton, setActiveButton] = useState(
     sessionStorage.getItem("buttonState") || "button1"
   );
-  const pageState = useSelector(selectSettingPageStateValue);
   const dispatch = useDispatch();
   const location = useLocation();
+  const searchState = useSelector(selectSettingSearchValue);
 
   const handleClick = (buttonId) => {
+    if (searchState) dispatch(setSearchState(false));
+
     setActiveButton(buttonId);
   };
   const handleOnClick = () => {
     dispatch(setBothPages(1));
+    if (searchState) dispatch(setSearchState(false));
     setActiveButton("button1");
     sessionStorage.setItem("buttonState", "button1");
     sessionStorage.setItem("moviesPageNr", 1);
