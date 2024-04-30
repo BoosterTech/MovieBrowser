cc411729b25e58345584fa710b2295daf5a2d180
Arkadiusz0809 <arek.torun12@gmail.com>
2024-04-30 23:52:02 +0200

    features for error in searching

--- src/common/noResult/styled.js
+++ src/common/noResult/styled.js
@@ -1,11 +1,33 @@
 import { styled } from "styled-components";
 
 export const MessageParagraph = styled.h1`
-  min-width: 674px;
-  height: 43px;
+  height: auto;
   font-weight: 600;
-  font-size:36px;
+  font-size: 36px;
   line-height: 43.2px;
   color: ${({ theme }) => theme.color.Black};
-  margin: 56px auto auto 276px;
+  margin: 56px auto 0 276px;
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.tabletHorizontalMax}px) {
+    margin: 56px auto;
+    text-align: center;
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMax}px) {
+    font-size: 30px;
+    margin: 40px auto;
+
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMedium}px) {
+    font-size: 24px;
+    padding: 20px;
+    margin: 20px auto;
+
+  }
+
+  @media (max-width: ${({ theme }) => theme.breakpoints.mobileMin}px) {
+    font-size: 18px;
+    padding: 20px;
+  }
 `;
