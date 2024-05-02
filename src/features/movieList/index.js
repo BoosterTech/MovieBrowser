67f6113d37e0d4e35785c0dc31e81a70622ac540
Darek Podczasik <boostertech@mail.com>
2024-05-02 21:58:20 +0100

    <ContentWrapper/> added

--- src/features/movieList/index.js
+++ src/features/movieList/index.js
@@ -126,7 +126,7 @@ const MovieListPage = () => {
 
   return loadingState === "loading" ? (
     searchState ? (
-      SearchPage(myQuery)
+      <ContentWrapper>{SearchPage(myQuery)}</ContentWrapper>
     ) : (
       <LoadingSpinner />
     )
