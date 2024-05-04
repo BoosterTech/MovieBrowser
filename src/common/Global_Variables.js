e589577a5c997622cebddf227b15b4841fdc5c1a
Darek Podczasik <boostertech@mail.com>
2024-05-04 14:42:34 +0100

    'Global_Variables' file created

--- src/common/Global_Variables.js
+++ src/common/Global_Variables.js
@@ -1,7 +1,10 @@
-export const apiBaseUrl = "https://api.themoviedb.org/3/";
-export const APIAuthorization =
+export const API_BASE_URL = "https://api.themoviedb.org/3/";
+export const API_AUTHORIZATION =
   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjI2MDE5NmJjOWJiYjBkZjdiZDc0N2NmMDEzNTdjMCIsInN1YiI6IjY2MDAwZmZjNjJmMzM1MDE2NDUxNWJhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j5BdMVijb7g_8sAaxu9WLzzFLGen6qB1lNYLha-96Tw";
-export const apiMoviePopularURL =
+export const API_MOVIE_POPULAR_URL =
   "https://api.themoviedb.org/3/movie/popular?language=en-US&page=";
-export const apiPeoplePopularURL =
+export const API_PEOPLE_POPULAR_URL =
   "https://api.themoviedb.org/3/person/popular?language=en-US&page=";
+
+export const DEFAULT_DEBOUNCE_TIME = 500;
+export const SEARCH_RESULTS_TITLE = "Search Results for";
