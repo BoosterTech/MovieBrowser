a6f2634718e15a77e041e594239550172063fa3e
Darek Podczasik <boostertech@mail.com>
2024-06-08 11:56:56 +0100

    popular movies api renamed, ApiPopularMoviesWithParams added, fetchOptions added

--- src/common/globalVariables.js
+++ src/common/globalVariables.js
@@ -1,8 +1,12 @@
 export const API_BASE_URL = "https://api.themoviedb.org/3/";
 export const API_AUTHORIZATION =
   "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjI2MDE5NmJjOWJiYjBkZjdiZDc0N2NmMDEzNTdjMCIsInN1YiI6IjY2MDAwZmZjNjJmMzM1MDE2NDUxNWJhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j5BdMVijb7g_8sAaxu9WLzzFLGen6qB1lNYLha-96Tw";
-export const API_MOVIE_POPULAR_URL =
+
+export const ApiPopularMovies =
   "https://api.themoviedb.org/3/movie/popular?language=en-US&page=";
+export const ApiPopularMoviesWithParams =
+  "https://api.themoviedb.org/3/search/movie";
+
 export const API_PEOPLE_POPULAR_URL =
   "https://api.themoviedb.org/3/person/popular?language=en-US&page=";
 export const URLImages = "https://image.tmdb.org/t/p/";
@@ -11,3 +15,10 @@ export const DEFAULT_DEBOUNCE_TIME = 500;
 export const SEARCH_RESULTS_TITLE = "Search Results for";
 export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
 
+export const fetchOptions = {
+  method: "GET",
+  headers: {
+    Authorization: API_AUTHORIZATION,
+    Accept: "application/json",
+  },
+};
