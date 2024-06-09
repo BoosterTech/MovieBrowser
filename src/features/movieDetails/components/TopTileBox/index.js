dc7e148218e259ad3ef35e72fccd247c8ae3f452
Darek Podczasik <boostertech@mail.com>
2024-06-09 16:18:21 +0100

    TrailerPlayer, WatchTrailerbutton & CloseTrailerButton components added

--- src/features/movieDetails/components/TopTileBox/index.js
+++ src/features/movieDetails/components/TopTileBox/index.js
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import { ReactComponent as Star } from "./../../../../assets/images/star.svg";
+
 import {
   PosterContainer,
   GradientCover,
@@ -11,32 +13,67 @@ import {
   RateContainer,
   VoteContainer,
   ScoreLimit,
+  TrailerPlayer,
+  WatchTrailerButton,
+  CloseTrailerButton,
 } from "./styled";
 
-export const TopTileBox = ({ imageSrc, title, rate, vote }) => (
-  <PosterContainer>
-    <Poster
-      $background={imageSrc}
-      alt={title} >
-    <GradientCover />
-    <Vignette />
-    <TitleWrapper>
-      <Title>{title}</Title>
-      <RatingWrapper>
-        <IconStar as={Star} />
-        <RateContainer>
-          {rate
-            .toFixed(1)
-            .toString()
-            .replace(".", ",")
-          }
-        </RateContainer>
-        <ScoreLimit>/ 10</ScoreLimit>
-        <VoteContainer>
-          {vote} {vote > 1 ? "votes" : "vote"}
-        </VoteContainer>
-      </RatingWrapper>
-    </TitleWrapper>
-    </Poster>
-  </PosterContainer>
-);
+export const TopTileBox = ({ imageSrc, title, rate, vote, trailerKey }) => {
+  const [trailerOpen, setTrailerOpen] = useState(false);
+
+  const handleTrailerOpen = () => {
+    trailerOpen ? setTrailerOpen(false) : setTrailerOpen(true);
+  };
+
+  const handleTrailerClose = () => {
+    setTrailerOpen(false);
+  };
+
+  return (
+    <PosterContainer>
+      <Poster $background={imageSrc} alt={title}>
+        <GradientCover />
+        <Vignette />
+        <CloseTrailerButton onClick={handleTrailerClose} $display={trailerOpen}>
+          ❌{" "}
+        </CloseTrailerButton>
+        {trailerOpen ? (
+          <TrailerPlayer
+            $show={trailerOpen}
+            url={`https://www.youtube.com/embed/${trailerKey}?autoplay=1`}
+            playing={true}
+            controls={true}
+            width="100%"
+            height="100%"
+            style={{
+              position: "absolute",
+              top: "50%",
+              left: "50%",
+              transform: "translate(-50%, -50%)",
+            }}
+          />
+        ) : (
+          <TitleWrapper>
+            <WatchTrailerButton
+              onClick={handleTrailerOpen}
+              $position={trailerOpen}
+            >
+              Watch Trailer
+            </WatchTrailerButton>
+            <Title>{title}</Title>
+            <RatingWrapper>
+              <IconStar as={Star} />
+              <RateContainer>
+                {rate.toFixed(1).toString().replace(".", ",")}
+              </RateContainer>
+              <ScoreLimit>/ 10</ScoreLimit>
+              <VoteContainer>
+                {vote} {vote > 1 ? "votes" : "vote"}
+              </VoteContainer>
+            </RatingWrapper>
+          </TitleWrapper>
+        )}
+      </Poster>
+    </PosterContainer>
+  );
+};
