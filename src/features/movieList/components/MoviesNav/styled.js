e99466c70d9523327fb59d2b75ca8dacaba08129
Darek Podczasik <boostertech@mail.com>
2024-06-15 12:53:04 +0100

    <MovieNav/> styles initial commit

---
+++ src/features/movieList/components/MoviesNav/styled.js
@@ -0,0 +1,16 @@
+import styled from "styled-components";
+
+export const NavList = styled.ul`
+  display: flex;
+  text-align: center;
+  justify-content: flex-start;
+  gap: 65px;
+  list-style-type: none;
+  font-size: 2.2rem;
+  font-weight: 600;
+  border-radius: 5px;
+  padding: 10px 10px;
+  box-shadow: 0 0 12px 5px rgba(0, 0, 0, 0.1);
+`;
+
+export const NavItem = styled.li``;
