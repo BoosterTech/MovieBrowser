05bb43071e85b549598f3c7a58f62bcf5f4a39c2
Darek Podczasik <boostertech@mail.com>
2024-06-15 16:56:50 +0100

    handleClick of navItem added

--- src/features/movieList/components/MoviesNav/index.js
+++ src/features/movieList/components/MoviesNav/index.js
@@ -1,12 +1,36 @@
-import { NavItem, NavList } from "./styled";
+import { NavItem, NavLinkItem, NavList } from "./styled";
 import navBarItems from "./navBarItems";
+import {
+  selectSettingMovieStateValue,
+  setMovieState,
+} from "../../../../Redux_store/settingSlice";
+import { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 
 const MoviesNav = () => {
+  const dispatch = useDispatch();
+  const [activeItem, setActiveItem] = useState(
+    useSelector(selectSettingMovieStateValue)
+  );
+
+  const handleClick = (item) => {
+ 
+
+    dispatch(setMovieState(item));
+    setActiveItem(item);
+  };
+
   return (
     <div>
       <NavList>
-        {navBarItems.map((item) => (
-          <NavItem>{item}</NavItem>
+        {navBarItems.map((item, index) => (
+          <NavItem
+            key={index}
+            onClick={() => handleClick(item)}
+            $active={item.toLowerCase() === activeItem.toLowerCase()}
+          >
+            {item}
+          </NavItem>
         ))}
       </NavList>
     </div>
