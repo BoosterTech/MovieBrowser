eb4d4ab86c917f348434497b38564ea6a4bfdf4b
Darek Podczasik <boostertech@mail.com>
2024-06-15 17:35:55 +0100

    styles  foe hover & active updated

--- src/features/movieList/components/MoviesNav/styled.js
+++ src/features/movieList/components/MoviesNav/styled.js
@@ -1,44 +1,34 @@
-import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const NavList = styled.ul`
+
   position: relative;
   display: flex;
   align-items: center;
   justify-content: space-evenly;
-
   gap: 100px;
   list-style-type: none;
-  font-size: 2.2rem;
+ 
   font-weight: 600;
-  border-radius: 2px;
   padding: 30px 15px 30px 15px;
   margin-top: 0;
+  border-radius: 5px;
 `;
 
 export const NavItem = styled.li`
-  font-size: 1.3rem;
+  font-size: 1.4rem;
   color: ${({ $active }) => ($active ? "white" : "black")};
   padding: 10px 50px;
   border: 1px solid transparent;
   border-radius: 25px;
   text-align: center;
-  background-color: ${({ $active }) => ($active ? "black" : "transparent")};
+  background-color: ${({ $active }) =>
+    $active ? "darkorange" : "transparent"};
 
   transition: all 0.5s ease;
 
   &:hover {
-    border: 1px solid black;
+    border: 1px solid darkorange;
     cursor: pointer;
-  }
-`;
-
-export const NavLinkItem = styled(NavLink)`
-  text-decoration: none;
-
-  &.active ${NavItem} {
-    border: 1px solid black;
-
-    font-size: 2rem;
   }
 `;
