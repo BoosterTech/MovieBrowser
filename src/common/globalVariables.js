098c397bb96264355309e171bd67ba5ce917a80a
Darek Podczasik <boostertech@mail.com>
2024-06-15 17:39:57 +0100

    ApiTopRated added to globalVariables

--- src/common/globalVariables.js
+++ src/common/globalVariables.js
@@ -8,6 +8,8 @@ export const ApiPopularMoviesWithParams =
   "https://api.themoviedb.org/3/search/movie";
 export const ApiUpcomingMovies =
   "https://api.themoviedb.org/3/movie/upcoming?language=en-US&page=";
+export const ApiTopRatedMovies =
+  "https://api.themoviedb.org/3/movie/top_rated?language=en-US&page=";
 
 export const API_PEOPLE_POPULAR_URL =
   "https://api.themoviedb.org/3/person/popular?language=en-US&page=";
