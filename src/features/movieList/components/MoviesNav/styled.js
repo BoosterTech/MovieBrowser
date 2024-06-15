6b0b8f25617ecb551f26c9601ef13727efa847d6
Darek Podczasik <boostertech@mail.com>
2024-06-15 16:59:32 +0100

    styles for movies NavList & NavItem updated :)

--- src/features/movieList/components/MoviesNav/styled.js
+++ src/features/movieList/components/MoviesNav/styled.js
@@ -1,16 +1,44 @@
+import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
 export const NavList = styled.ul`
+  position: relative;
   display: flex;
-  text-align: center;
-  justify-content: flex-start;
-  gap: 65px;
+  align-items: center;
+  justify-content: space-evenly;
+
+  gap: 100px;
   list-style-type: none;
   font-size: 2.2rem;
   font-weight: 600;
-  border-radius: 5px;
-  padding: 10px 10px;
-  box-shadow: 0 0 12px 5px rgba(0, 0, 0, 0.1);
+  border-radius: 2px;
+  padding: 30px 15px 30px 15px;
+  margin-top: 0;
 `;
 
-export const NavItem = styled.li``;
+export const NavItem = styled.li`
+  font-size: 1.3rem;
+  color: ${({ $active }) => ($active ? "white" : "black")};
+  padding: 10px 50px;
+  border: 1px solid transparent;
+  border-radius: 25px;
+  text-align: center;
+  background-color: ${({ $active }) => ($active ? "black" : "transparent")};
+
+  transition: all 0.5s ease;
+
+  &:hover {
+    border: 1px solid black;
+    cursor: pointer;
+  }
+`;
+
+export const NavLinkItem = styled(NavLink)`
+  text-decoration: none;
+
+  &.active ${NavItem} {
+    border: 1px solid black;
+
+    font-size: 2rem;
+  }
+`;
