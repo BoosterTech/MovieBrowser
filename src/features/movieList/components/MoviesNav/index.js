cc6a4fbc7a852f1a3d4485879a8dece97b6d392e
Darek Podczasik <boostertech@mail.com>
2024-06-15 19:06:56 +0100

    hidding MoviesNav, while using search bar, added

--- src/features/movieList/components/MoviesNav/index.js
+++ src/features/movieList/components/MoviesNav/index.js
@@ -2,6 +2,7 @@ import { NavItem, NavList } from "./styled";
 import navBarItems from "./navBarItems";
 import {
   selectSettingMovieStateValue,
+  selectSettingSearchValue,
   setMovieState,
 } from "../../../../Redux_store/settingSlice";
 import { useEffect, useState } from "react";
@@ -10,11 +11,17 @@ import { useDispatch, useSelector } from "react-redux";
 const MoviesNav = () => {
   const dispatch = useDispatch();
   const movieStateValue = useSelector(selectSettingMovieStateValue);
+  const searchStateValue = useSelector(selectSettingSearchValue);
   const [activeItem, setActiveItem] = useState(movieStateValue);
+  const [searchState, setSearchState] = useState(searchStateValue);
 
   useEffect(() => {
     setActiveItem(movieStateValue);
   }, [movieStateValue]);
+
+  useEffect(() => {
+    setSearchState(searchStateValue);
+  }, [searchStateValue]);
 
   const handleClick = (item) => {
     dispatch(setMovieState(item));
@@ -22,19 +29,17 @@ const MoviesNav = () => {
   };
 
   return (
-    <div>
-      <NavList>
-        {navBarItems.map((item, index) => (
-          <NavItem
-            key={index}
-            onClick={() => handleClick(item)}
-            $active={item.toLowerCase() === activeItem.toLowerCase()}
-          >
-            {item}
-          </NavItem>
-        ))}
-      </NavList>
-    </div>
+    <NavList $visible={searchState}>
+      {navBarItems.map((item, index) => (
+        <NavItem
+          key={index}
+          onClick={() => handleClick(item)}
+          $active={item.toLowerCase() === activeItem.toLowerCase()}
+        >
+          {item}
+        </NavItem>
+      ))}
+    </NavList>
   );
 };
 
