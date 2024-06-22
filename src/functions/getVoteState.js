6577287dc8790360514fc18dbafb4b21f3d22b4c
Darek Podczasik <boostertech@mail.com>
2024-06-22 23:49:44 +0100

    getVotes finction updated with additional conditions

--- src/functions/getVoteState.js
+++ src/functions/getVoteState.js
@@ -1,9 +1,11 @@
-const getVoteState = (vote, display = true) => {
-  if (vote === 0) {
+const getVoteState = (rate, vote) => {
+  if (vote === 0 && rate !== 0) {
     return "No votes yet";
   }
-  if (display === true) return `${vote} ${vote > 1 ? "votes" : "vote"}`;
-  return `${vote > 1 ? "votes" : "vote"}`;
+  // if (display === true) return `${vote} ${vote > 1 ? "votes" : "vote"}`;
+  else if (vote !== 0) {
+    return `${vote > 1 ? "votes" : "vote"}`;
+  }
 };
 
 export default getVoteState;
