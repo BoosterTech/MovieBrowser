6d620de261393287c3c9db56935f44813080bbc9
Darek Podczasik <boostertech@mail.com>
2024-06-22 23:21:17 +0100

    GlobalBariables.js moved

---
+++ src/common/Global_Variables.js
@@ -0,0 +1,13 @@
+export const API_BASE_URL = "https://api.themoviedb.org/3/";
+export const API_AUTHORIZATION =
+  "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzYjI2MDE5NmJjOWJiYjBkZjdiZDc0N2NmMDEzNTdjMCIsInN1YiI6IjY2MDAwZmZjNjJmMzM1MDE2NDUxNWJhZiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.j5BdMVijb7g_8sAaxu9WLzzFLGen6qB1lNYLha-96Tw";
+export const API_MOVIE_POPULAR_URL =
+  "https://api.themoviedb.org/3/movie/popular?language=en-US&page=";
+export const API_PEOPLE_POPULAR_URL =
+  "https://api.themoviedb.org/3/person/popular?language=en-US&page=";
+export const URLImages = "https://image.tmdb.org/t/p/";
+export const backdropURL = `${URLImages}w1920_and_h800_multi_faces`;
+export const DEFAULT_DEBOUNCE_TIME = 500;
+export const SEARCH_RESULTS_TITLE = "Search Results for";
+export const IMAGE_BASE_URL = "https://image.tmdb.org/t/p/w500";
+
